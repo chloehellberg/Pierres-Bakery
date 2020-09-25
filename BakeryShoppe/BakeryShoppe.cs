b7af3701e90789d2d9f535052638eeea0f8e799c
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-25 14:51:49 -0700

    Refactoring to include different bread selections. Currently working on branching logic so that my method won't break.

--- BakeryShoppe/BakeryShoppe.cs
+++ BakeryShoppe/BakeryShoppe.cs
@@ -7,27 +7,49 @@ namespace BakeryShoppe
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery Shoppe! We offer the finest Breads and Pastries around. Would you like to purchase something? Enter y for yes and n for no.");
+      Console.WriteLine("Welcome to Pierre's Bakery Shoppe! We offer the finest Breads and Pastries around. Would you like to purchase something? Enter 'y' for yes and 'n' for no.");
       string userToShopInput = Console.ReadLine();
 
       if (userToShopInput == "y")
       {
-        Console.WriteLine("How many loaves of bread would you like today?");
+        Console.WriteLine("------------------------------------------------------------------------------");
+        Console.WriteLine("Let's start with our bread selection. We offer two types: Rye and Sourdough."); 
+        Console.WriteLine("Please enter what bread type you would like first: ");
+        string firstBreadType = Console.ReadLine().ToLower();
+        
+        Console.WriteLine($"Very good. And how many loaves of {firstBreadType} would you like today?");
         string breadAmountRequested = Console.ReadLine();
         int breadAmount = int.Parse(breadAmountRequested);
 
-      
-        Console.WriteLine("Very good. And would you like to purchase any pastries? Please enter the amount you would like.");
-        string pastryAmountRequested = Console.ReadLine();
-        int pastryAmount = int.Parse(pastryAmountRequested);
+        Console.WriteLine("Would you like to select the other bread type as well? Please type 'y' for yes and 'n' for no.");
+        string userSecondBreadSelection = Console.ReadLine();
+        if (userSecondBreadSelection == "y")
+        {
+          Console.WriteLine("And how many loaves of that would you like?");
+          string breadAmountRequestedSecond = Console.ReadLine();
+          int breadAmountSecond = int.Parse(breadAmountRequestedSecond);
 
-        Bread breadOrder = new Bread(breadAmount);
-        Pastry pastryOrder = new Pastry(pastryAmount);
-        int totalCost = breadOrder.CalculateBread() + pastryOrder.CalculatePastry();
+          Console.WriteLine("Very good. And would you like to purchase any pastries? Please enter the amount you would like.");
+          string pastryAmountRequested = Console.ReadLine();
+          int pastryAmount = int.Parse(pastryAmountRequested);
 
-        Console.WriteLine("Thank you for your purchases. Your total today will be $" + totalCost);
-        Console.WriteLine("Have a nice day, and come again!");
+          Bread breadOrder = new Bread(breadAmount);
+          Pastry pastryOrder = new Pastry(pastryAmount);
+          int totalCost = breadOrder.CalculateBread() + pastryOrder.CalculatePastry();
 
+          Console.WriteLine("Thank you for your purchases. Your total today will be $" + totalCost + ". Have a nice day, and come again!");
+        }
+        else {
+          Console.WriteLine("Very good. And would you like to purchase any pastries? Please enter the amount you would like.");
+          string pastryAmountRequested = Console.ReadLine();
+          int pastryAmount = int.Parse(pastryAmountRequested);
+
+          Bread breadOrder = new Bread(breadAmount);
+          Pastry pastryOrder = new Pastry(pastryAmount);
+          int totalCost = breadOrder.CalculateBread() + pastryOrder.CalculatePastry();
+
+          Console.WriteLine("Thank you for your purchases. Your total today will be $" + totalCost + ". Have a nice day, and come again!");
+        }
       }
       else
       {
@@ -36,3 +58,40 @@ namespace BakeryShoppe
     }
   }
 }
+
+// namespace BakeryShoppe
+// {
+//   public class Program
+//   {
+//     public static void Main()
+//     {
+//       Console.WriteLine("Welcome to Pierre's Bakery Shoppe! We offer the finest Breads and Pastries around. Would you like to purchase something? Enter y for yes and n for no.");
+//       string userToShopInput = Console.ReadLine();
+
+//       if (userToShopInput == "y")
+//       {
+//         Console.WriteLine("How many loaves of bread would you like today?");
+//         string breadAmountRequested = Console.ReadLine();
+//         int breadAmount = int.Parse(breadAmountRequested);
+
+      
+//         Console.WriteLine("Very good. And would you like to purchase any pastries? Please enter the amount you would like.");
+//         string pastryAmountRequested = Console.ReadLine();
+//         int pastryAmount = int.Parse(pastryAmountRequested);
+
+//         Bread breadOrder = new Bread(breadAmount);
+//         Pastry pastryOrder = new Pastry(pastryAmount);
+//         int totalCost = breadOrder.CalculateBread() + pastryOrder.CalculatePastry();
+
+//         Console.WriteLine("Thank you for your purchases. Your total today will be $" + totalCost);
+//         Console.WriteLine("Have a nice day, and come again!");
+
+//       }
+//       else
+//       {
+//         Console.WriteLine("Then have a nice day!");
+//       }
+//     }
+//   }
+// }
+
